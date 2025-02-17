7864cf4907121712550921a48bd1178f0d50f8dc
Unity <migue.navega227@gmail.com>
2025-02-17 10:41:05 -0600

    Subiendo mi Archivo

--- src/config/navData.json.ts
+++ src/config/navData.json.ts
@@ -8,44 +8,28 @@ export interface navDropdownItem {
   text: string;
   dropdown: navLinkItem[];
 }
-
 export type navItem = navLinkItem | navDropdownItem;
 
 // note: 1 level of dropdown is supported
 const navConfig: navItem[] = [
   {
-    text: "Home",
+    text: "Inicio",
     link: "/",
   },
   {
-    text: "portfolios",
+    text: "Portafolios",
     link: "/portfolio",
   },
   {
-    text: "Pricing",
+    text: "Paquetes",
     link: "/#pricing",
   },
+
   {
-    text: "Pages",
-    dropdown: [
-      {
-        text: "Portfolio",
-        link: "/portfolio/couple-1",
-      },
-      {
-        text: "Legal",
-        link: "/privacy-policy/",
-      },
-      {
-        text: "Elements",
-        link: "/elements/",
-      },
-      {
-        text: "404",
-        link: "/not-a-link/",
-      },
-    ],
+    text: "Contactame",
+    link: "/#contact",
   },
+ 
 ];
 
 export default navConfig;
